86fc7a0072ae9f93ea5f935c4f5a31c98794827a
Heesu Kim <63194988+lucakim27@users.noreply.github.com>
2020-12-19 12:51:21 +0900

    Create covidkorea.py

---
+++ covidkorea.py
@@ -0,0 +1,36 @@
+import requests
+import bs4
+
+
+class covid_korea:
+    def __init__(self):
+        pass
+
+    def total_cases(self):
+        website = "http://ncov.mohw.go.kr/en/bdBoardList.do?brdId=16&brdGubun=162&dataGubun=&ncvContSeq=&contSeq=&board_id= "
+        result = requests.get(website)
+        soup = bs4.BeautifulSoup(result.text, "lxml")
+        cases = soup.select('.num')
+
+        print("Total: " + str(cases[18].text))
+        print("Seoul: " + str(cases[0].text))
+        print("Gyeonggi-do: " + str(cases[8].text))
+        print("Incheon: " + str(cases[3].text))
+        print("Sejong: " + str(cases[7].text))
+        print("Gangwon-do: " + str(cases[9].text))
+        print("Chungcheongbuk-do: " + str(cases[10].text))
+        print("Chungcheongnam-do: " + str(cases[11].text))
+        print("Daejeon: " + str(cases[5].text))
+        print("Gyeongsangbuk-do: " + str(cases[14].text))
+        print("Gyeongsangnam-do: " + str(cases[15].text))
+        print("Busan: " + str(cases[1].text))
+        print("Ulsan: " + str(cases[6].text))
+        print("Daegu: " + str(cases[2].text))
+        print("Jeollabuk-do: " + str(cases[12].text))
+        print("Jeollanam-do: " + str(cases[13].text))
+        print("Gwangju: " + str(cases[4].text))
+        print("Jeju: " + str(cases[16].text))
+
+
+c = covid_korea()
+c.total_cases()
