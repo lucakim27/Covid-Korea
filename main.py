7eb2c980a5455b9f8089f72fc6e62fa8a74f8496
Heesu Kim <63194988+lucakim27@users.noreply.github.com>
2021-02-07 17:15:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 from covidkorea import covid_korea
 
 c = covid_korea()
-
-c.total_cases()
-
-print('\n')
-
-c.new_daily_cases()
-
-print('\n')
-
-c.gwangju_travel_path()
-
-print('\n')
-
-c.gwangju_prevention_area()
